60e21a54cbfc1185d5c8792518e2887c7ec68b75
Damassss <pescariaurbana@gmail.com>
2025-09-24 22:26:08 -0300

    C

--- pdvpostocombustivel/pdvpostocombustivel/src/main/java/com/br/pdvpostocombustivel/domain/repository/PessoaRepository.java
+++ pdvpostocombustivel/pdvpostocombustivel/src/main/java/com/br/pdvpostocombustivel/domain/repository/PessoaRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+    
     Optional<Pessoa> findByNomeCompleto(String nomeCompleto);
 
     Optional<Pessoa> findByCpfCnpj(String cpfCnpj);
